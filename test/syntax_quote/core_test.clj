479567aaca8778188ba575080aa6942f0d1fc8ae
Kevin Downey <redchin@gmail.com>
2011-11-10 14:51:39 -0500

    align tests

--- test/syntax_quote/core_test.clj
+++ test/syntax_quote/core_test.clj
@@ -4,16 +4,18 @@
 
 (deftest t-syntax-quote
   (are [x y] (= x y)
-       `(1 2 3) (syntax-quote (1 2 3))
-       `foo (syntax-quote foo)
-       `Boolean/TYPE (syntax-quote Boolean/TYPE)
-       `foo/bar (syntax-quote foo/bar)
-       `[foo bar baz] (syntax-quote [foo bar baz])
-       `~+ (syntax-quote (syntax-unquote +))
-       [1 2 3 4] (syntax-quote
-                  [1 2 (syntax-unquote-splicing
-                        [3 4])])
-       )
+       `(1 2 3)            (syntax-quote (1 2 3))
+       `foo                (syntax-quote foo)
+       `Boolean/TYPE       (syntax-quote Boolean/TYPE)
+       `foo/bar            (syntax-quote foo/bar)
+       `[foo bar baz]      (syntax-quote [foo bar baz])
+       `~+                 (syntax-quote (syntax-unquote +))
+       [1 2 3 4]           (syntax-quote
+                            [1 2 (syntax-unquote-splicing
+                                  [3 4])])
+       `{:a [1 2 ~@[3 4]]} (syntax-quote
+                            {:a [1 2 (syntax-unquote-splicing
+                                      [3 4])]}))
   (is (not (= 'foo (syntax-quote foo#))))
   (let [[f1 f2] (syntax-quote [foo# foo#])]
     (is (= f1 f2))))
