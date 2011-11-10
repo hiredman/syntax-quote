37a4ab503dbb13b09438a3f391f80746288cfb07
Kevin Downey <redchin@gmail.com>
2011-11-10 16:25:00 -0500

    add sonatype snapshot repo

--- project.clj
+++ project.clj
@@ -1,3 +1,4 @@
 (defproject syntax-quote "1.0.0-SNAPSHOT"
+  :repositories {"sona" "http://oss.sonatype.org/content/repositories/snapshots"}
   :description "FIXME: write"
   :dependencies [[clojure "1.4.0-master-SNAPSHOT"]])
